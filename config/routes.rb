5e285575928522ff8789f344a912cb727141c18f
ric <ricyoung@gmail.com>
2013-07-12 16:39:34 -0700

    added user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Treebook::Application.routes.draw do
-  resources :statuses
+  devise_for :users
 
+  resources :statuses
+   root to: 'statuses#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,4 +60,5 @@ Treebook::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
